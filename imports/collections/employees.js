8bb1c78427d42cc68c0d2f754518081d74168fb9
Will Fong <will.fong@qq.com>
2016-06-04 11:35:01 +0800

    Push

---
+++ imports/collections/employees.js
@@ -0,0 +1,3 @@
+import {Mongo} from 'meteor/mongo';
+
+export const Employees = new Mongo.collection('employees');
---
+++ server/main.js
@@ -0,0 +1,27 @@
+import _ from 'lodash';
+import {Meteor} from 'meteor/meteor';
+import {Employees} from '../imports/collections/employees';
+import {image, helpers} from 'faker';
+
+
+Meteor.startup(() => {
+
+  const numberRecords = Employees.find({}).count();
+
+  if (!numberRecords) {
+    console.log('Need to create records...');
+    _.times(5000, () => {
+      const {name, email, phone} = helpers.createCard();
+      Employees.insert({
+        name: name,
+        email: email,
+        phone: phone,
+        avatar: image.avatar()
+      });
+    });
+  } else {
+    console.log('Total items:' + numberRecords);
+  }
+
+
+});
